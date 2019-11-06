9e419b62b070715fbe5c2f770c2bcb1827eb167f
klinga <klingaroo@gmail.com>
2019-11-06 15:04:34 -0500

    basic store transactions

---
+++ rebalancer/datastore_transactions.py
@@ -0,0 +1,33 @@
+"""
+Datastore transaction methods
+"""
+
+
+def insert_or_ignore(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if not instance:
+        instance = model(**kwargs)
+        session.add(instance)
+    return instance
+
+
+def delete_record(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).one()
+    session.delete(instance)
+
+
+def update_record(session, model, sid, **kwargs):
+    instance = session.query(model).filter_by(sid=sid).one()
+    for key, value in kwargs.items():
+        setattr(instance, key, value)
+
+
+def retrieve_record(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    return instance
+
+
+def retrieve_records(session, model, **kwargs):
+    instances = session.query(model).filter_by(**kwargs).order_by(
+        model.sid).all()
+    return instances
