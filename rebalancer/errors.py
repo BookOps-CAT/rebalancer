7ebcaee3eb02202b98d1f3c498de064d29ba49ab
klinga <klingaroo@gmail.com>
2019-11-15 14:48:47 -0500

    init commit

---
+++ rebalancer/errors.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module contains the set of Rebalancer's exceptions.
+"""
+
+
+class RebalancerError(Exception):
+    """Base class for exceptions in this module."""
+    pass
