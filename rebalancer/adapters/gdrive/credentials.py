7c7e7a9840ebc8115d30fe0b728446e7ad921d70
klinga <klingaroo@gmail.com>
2019-11-19 14:37:31 -0500

    flow from_client_secrets_file

--- rebalancer/adapters/gdrive/credentials.py
+++ rebalancer/adapters/gdrive/credentials.py
@@ -17,7 +17,7 @@ def get_access_token():
         with open(pickled_token, 'rb') as token:
             creds = pickle.load(token)
 
-    if not creds and not creds.valid:
+    if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             print('refresh')
             creds.refresh(Request())
@@ -28,27 +28,12 @@ def get_access_token():
                 '.google/rebalancer-dev-credentials.json')
             flow = InstalledAppFlow.from_client_secrets_file(
                 creds_fh,
-                scopes=['https://www.googleapis.com/auth/drive.metadata.readonly']) # will this limit scopes enabled in creds??
+                scopes=[
+                    'https://www.googleapis.com/auth/drive',
+                    'https://www.googleapis.com/auth/spreadsheets'])
             creds = flow.run_local_server(port=0)
 
         with open(pickled_token, 'wb') as token:
             pickle.dump(creds, token)
 
     return creds
-
-
-if __name__ == '__main__':
-    from googleapiclient.discovery import build
-
-    creds = get_access_token()
-    service = build('drive', 'v3', credentials=creds)
-    results = service.files().list(
-        pageSize=10, fields='nextPageToken, files(id, name)').execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No files found')
-    else:
-        print('Files:')
-        for item in items:
-            print(f'{item["name"]}, {item["id"]}')
